3e66324abd92f073a5e5cc27268828a809e6af3f
8dgkim <0dgkim@gmail.com>
2023-01-29 18:25:00 -0500

    timedisplay v2 with date

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:intl/intl.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -12,29 +11,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Time',
+    return const MaterialApp(
+      title: 'Date and Time',
       home: TimeDisplay(),
-      // home: Clock(),
     );
   }
 }
 
-
 class TimeDisplay extends StatefulWidget {
+  const TimeDisplay({super.key});
+
   @override
+  // ignore: library_private_types_in_public_api
   _TimeDisplayState createState() => _TimeDisplayState();
 }
 
 class _TimeDisplayState extends State<TimeDisplay> {
   late Timer _timer;
-  StreamController<DateTime> _timeController = StreamController<DateTime>();
+  final StreamController<DateTime> _timeController = StreamController<DateTime>();
 
   @override
   void initState() {
     super.initState();
     _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
-      _timeController.add(DateTime.now());
+      _timeController.add(DateTime.now().toLocal());
     });
   }
 
@@ -49,22 +49,34 @@ class _TimeDisplayState extends State<TimeDisplay> {
   Widget build(BuildContext context) {
     return StreamBuilder<DateTime>(
       stream: _timeController.stream,
-      initialData: DateTime.now(),
+      initialData: DateTime.now().toLocal(),
       builder: (context, snapshot) {
         return Container(
           alignment: Alignment.center,
-          child: Text(
-            "${snapshot.data?.hour.toString().padLeft(2, '0')}:${snapshot.data?.minute.toString().padLeft(2, '0')}:${snapshot.data?.second.toString().padLeft(2, '0')}:${snapshot.data?.millisecond.toString().padLeft(3, '0')}",
-            style: const TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-              decoration: TextDecoration.none,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                DateFormat("yyyy:MM:dd").format(snapshot.data!),
+                style: const TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+              const SizedBox(height: 15),
+              Text(
+                "${snapshot.data?.hour.toString().padLeft(2, '0')}:${snapshot.data?.minute.toString().padLeft(2, '0')}:${snapshot.data?.second.toString().padLeft(2, '0')}:${snapshot.data?.millisecond.toString().padLeft(3, '0')}",
+                style: const TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ],
           ),
         );
       },
     );
   }
 }
-
-
